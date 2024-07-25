4ffdef9f0e573668a66e86aaa3cc76fedf4fe62f
Bùi Hữu Nghĩa <130369258+buihuunghia2004@users.noreply.github.com>
2024-07-25 17:00:28 +0700

    update

--- src/navigations/Navigator.js
+++ src/navigations/Navigator.js
@@ -1,16 +1,16 @@
-import {createStackNavigator} from '@react-navigation/stack';
-import {stackName} from './screens';
+import { createStackNavigator } from '@react-navigation/stack';
+import { stackName } from './screens';
 import Counter from '../example/Counter';
 import RegisterScreen from '../screen/RegisterScreen';
+
 
 
 const Stack = createStackNavigator();
 
 export function Navigator() {
   return (
-    <Stack.Navigator screenOptions={{headerShown: false}}>
-       <Stack.Screen name={stackName.example} component={Counter} />
-      <Stack.Screen name={stackName.register} component={RegisterScreen} />
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name={stackName.register} component={Counter} />
     </Stack.Navigator>
   );
 }
