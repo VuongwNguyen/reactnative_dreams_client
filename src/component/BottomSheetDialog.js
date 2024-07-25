fd8b16dbf9220b0a42e5ec63300350f107824023
VietTien198 <141416494+VietTien198@users.noreply.github.com>
2024-07-25 16:26:36 +0700

    bottomSheet

--- src/component/BottomSheetDialog.js
+++ src/component/BottomSheetDialog.js
@@ -2,14 +2,14 @@ import { Dimensions, Pressable, StyleSheet } from 'react-native'
 import React from 'react'
 import Animated, { FadeIn, FadeOut, SlideInDown, SlideOutDown } from 'react-native-reanimated'
 import AppButton from './AppButton'
-import { Icon, Text, TouchableOpacity } from 'react-native-ui-lib'
+import { Icon, Text, TouchableOpacity, View } from 'react-native-ui-lib'
 
 const AnimatedPressable = Animated.createAnimatedComponent(Pressable)
 
 const { width, height } = Dimensions.get("window")
 
 const BottomSheetDialog = (props) => {
-    const { onPressClose, title } = props
+    const { onPressClose, textTitle, styleTextTitle, textContent, styleTextContent, children } = props
     return (
         <>
             <AnimatedPressable
@@ -21,8 +21,12 @@ const BottomSheetDialog = (props) => {
                 <TouchableOpacity onPress={onPressClose}>
                     <Icon source={require("../../assets/icons/vector.png")} />
                 </TouchableOpacity>
-                <Text text20T>Title</Text>
-
+                <View style={{ height: 20 }} />
+                <Text text40 style={styleTextTitle}>{textTitle}</Text>
+                <View style={{ height: 15 }} />
+                <Text text70R color={"#6c757d"} style={styleTextContent}>{textContent}</Text>
+                <View style={{ height: 15 }} />
+                {children}
             </Animated.View>
         </>
     )
--- src/example/Counter.js
+++ src/example/Counter.js
@@ -10,11 +10,18 @@ import Animated, {
   useAnimatedStyle,
   withTiming,
 } from 'react-native-reanimated';
+import AppEditText from '../component/AppEditText';
+import AppButton from '../component/AppButton';
+import { Button } from 'react-native-ui-lib';
+import BottomSheetDialog from '../component/BottomSheetDialog';
 
 export default function Counter() {
   const count = useSelector(state => state.count);
   const { t } = useTranslation();
   const dispatch = useDispatch();
+  const [password, setPassword] = useState("");
+  const [showBottomSheet, setShowBottomSheet] = useState(false)
+
 
   const changeLanguage = () => {
     if (i18n.language === 'vi') {
@@ -26,20 +33,85 @@ export default function Counter() {
 
   return (
     <View style={style.container}>
-      <Text style={style.counter}>
-        {t('counter')}: {count}
-      </Text>
-      <TouchableOpacity style={style.button} onPress={() => dispatch(inc())}>
-        <Text>Increment</Text>
-      </TouchableOpacity>
+      <AppEditText
+        styleContainer={{ width: "100%" }}
+        style={{ fontSize: 16 }}
+        floatingPlaceholderStyle={{ fontSize: 16 }}
+        placeholder={"Password"}
+        value={password}
+        onChangeText={(text) => { setPassword(text) }}
+        isPassword
+      />
+      <View style={{ height: 40 }} />
+      <AppButton
+        onPress={() => { console.log("Hello word") }}
+        labelStyle={{ fontSize: 15, fontWeight: "700" }}
+        label={"Save"}
+        borderRadius={8}
+        size={Button.sizes.small}
+      />
+      <View style={{ height: 40 }} />
+      <AppButton
+        onPress={() => { console.log("Hello word") }}
+        buttonStyle={{
+          width: 342, height: 70
+        }}
+        labelStyle={{ fontSize: 19, fontWeight: "700" }}
+        label={"Hello Word"}
+        borderRadius={15}
+      /><View style={{ height: 40 }} />
+      <AppButton
+        onPress={() => { setShowBottomSheet(true) }}
+        buttonStyle={{
+          width: 342, height: 70,
 
-      <TouchableOpacity style={style.button} onPress={() => dispatch(dec())}>
-        <Text>Decrement</Text>
-      </TouchableOpacity>
+        }}
+        labelStyle={{ fontSize: 19, fontWeight: "700" }}
+        label={"Show BottomSheet"}
+        borderRadius={30}
+      />
+      {
+        showBottomSheet && (
+          <BottomSheetDialog
+            onPressClose={() => { setShowBottomSheet(false) }}
+            textTitle={"Name"}
+            textContent={"Personal information must not include third-party contact information, advertising, content, pornography, and other illegal noioij dunng to avoid account closure"}
+          >
+            <View style={{ flexDirection: "row" }}>
+              <AppEditText
+                styleContainer={{ flex: 1 }}
+                style={{ fontSize: 16 }}
+                floatingPlaceholderStyle={{ fontSize: 16 }}
+                placeholder={"Password"}
+                value={password}
+                onChangeText={(text) => { setPassword(text) }}
+                isPassword
+              />
+              <View style={{ width: 15 }} />
+              <AppEditText
+                styleContainer={{ flex: 1 }}
+                style={{ fontSize: 16 }}
+                floatingPlaceholderStyle={{ fontSize: 16 }}
+                placeholder={"Password"}
+                value={password}
+                onChangeText={(text) => { setPassword(text) }}
+                isPassword
+              />
+            </View>
+            <View style={{ height: 30 }} />
+            <AppButton
+              onPress={() => { console.log("Hello word") }}
+              buttonStyle={{
+                width: 342, height: 60
+              }}
+              labelStyle={{ fontSize: 19, fontWeight: "700" }}
+              label={"Hello Word"}
+              borderRadius={15}
+            />
 
-      <TouchableOpacity style={style.button} onPress={changeLanguage}>
-        <Text>Change Language</Text>
-      </TouchableOpacity>
+          </BottomSheetDialog>
+        )
+      }
     </View>
   );
 }
