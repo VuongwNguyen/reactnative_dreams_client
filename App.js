816655be38b664260663f3fd58f9aa2614098456
Nguyen Ba Sang <nguyenbasang19122004@gmail.com>
2024-07-27 23:57:57 +0700

    feat: splash screen
    fix: resolve conflicts
    
    fix: resolve conflict
    
    fix: resolve conflicts

--- App.js
+++ App.js
@@ -2,22 +2,32 @@ import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {Navigator} from './src/navigations/Navigator';
+import SplashScreen from 'react-native-splash-screen';
 import {Provider as ReduxProvider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
+import {Navigator} from './src/navigations/Navigator';
 import {persistor, store} from './src/store';
-import SplashScreen from 'react-native-splash-screen';
 
 export default function App() {
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      SplashScreen.hide();
+    }, 2000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
   return (
     <ReduxProvider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <SafeAreaProvider>
           <GestureHandlerRootView>
             <NavigationContainer
-              onReady={() => {
-                SplashScreen.hide();
-              }}>
+            // onReady={() => {
+            //   SplashScreen.hide();
+            // }}
+            >
               <Navigator />
             </NavigationContainer>
           </GestureHandlerRootView>
--- src/example/Counter.js
+++ src/example/Counter.js
@@ -1,5 +1,5 @@
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { dec, inc } from '../store/slices/example';
 import { useTranslation } from 'react-i18next';
@@ -10,14 +10,11 @@ import Animated, {
   useAnimatedStyle,
   withTiming,
 } from 'react-native-reanimated';
-import EditText from '../component/EditText';
 
 export default function Counter() {
   const count = useSelector(state => state.count);
   const { t } = useTranslation();
   const dispatch = useDispatch();
-  const [password, setPassword] = useState("")
-
 
   const changeLanguage = () => {
     if (i18n.language === 'vi') {
@@ -29,33 +26,41 @@ export default function Counter() {
 
   return (
     <View style={style.container}>
-      <View style={{ flexDirection: 'row' }}>
-        <EditText
-          styleContainer={{ flex: 1 }}
-          style={{ width: "100%", fontSize: 16 }}
-          floatingPlaceholderStyle={{ fontSize: 16 }}
-          placeholder={"Password"}
-          value={password}
-          onChangeText={(text) => { setPassword(text) }}
-          isPassword
-        />
-        <EditText
-          styleContainer={{ flex: 1 }}
-          style={{ width: "100%", fontSize: 16 }}
-          floatingPlaceholderStyle={{ fontSize: 16 }}
-          placeholder={"Password"}
-          value={password}
-          onChangeText={(text) => { setPassword(text) }}
-          isPassword
-        />
-      </View>
+      <Text style={style.counter}>
+        {t('counter')}: {count}
+      </Text>
+      <TouchableOpacity style={style.button} onPress={() => dispatch(inc())}>
+        <Text>Increment</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={style.button} onPress={() => dispatch(dec())}>
+        <Text>Decrement</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={style.button} onPress={changeLanguage}>
+        <Text>Change Language</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const style = StyleSheet.create({
+  counter: {
+    fontWeight: 'bold',
+    color: 'black',
+    fontSize: 16,
+    marginBottom: 8,
+  },
+  button: {
+    backgroundColor: 'orange',
+    width: '50%',
+    padding: 16,
+    marginBottom: 8,
+    borderRadius: 16,
+  },
   container: {
     flex: 1,
+    padding: 16,
     justifyContent: 'center',
     alignItems: 'center',
   },
