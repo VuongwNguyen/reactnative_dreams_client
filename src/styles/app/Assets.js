a0a0f262e18dec6179e41369e29f45516463ef45
Nguyen Quoc Vuong <nguyenvuongw134@gmail.com>
2024-07-29 09:36:50 +0700

    fix: changed Asset variable (#10)

--- src/styles/app/Assets.js
+++ src/styles/app/Assets.js
@@ -1,26 +1,26 @@
 
-/* icon assets by fontawesome library */
+/* icon assets by fontawesome6 library */
 export const Assets = {
     icon: {
-        close: 'fas fa-times-circle',
-        eyeClose: 'fas fa-eye-slash',
-        eyeOpen: 'fas fa-eye',
-        search: 'fas fa-search',
-        user: 'fas fa-user',//
-        back: 'fas fa-arrow-left',
-        nationaly: 'fas fa-flag',
-        notification: 'fas fa-bell',
-        message: 'fas fa-envelope',
-        logout: 'fas fa-sign-out-alt',
-        home: 'fas fa-home',
-        translate: 'fas fa-language',
-        edit: 'fas fa-edit',
-        position: 'fas fa-map-marker-alt',
-        gender: 'fas fa-venus-mars',
-        terms: 'fas fa-file-alt',
-        privacy: 'fas fa-lock',
-        theme: 'fas fa-moon',
-        birthday: 'fas fa-birthday-cake',
+        close: 'close',
+        eyeClose: 'eye-slash',
+        eyeOpen: 'eye',
+        search: 'search',// feather
+        user: 'user',
+        back: 'arrow-left',// feather
+        nationaly: 'earth-asia',
+        notification: 'bell', 
+        message: 'comment', // foundation
+        logout: 'log-out',// feather 
+        home: 'home', // entypo
+        translate: 'translate', // materialIcons
+        edit: 'edit', // materialIcons
+        location: 'location',// EvilIcons
+        gender: 'transgender', // fontAwesome
+        terms: 'archie', // feather
+        privacy: 'lock',
+        theme: 'theme-light-dark', // MaterialCommunityIcons
+        birthday: 'birthday-cake',// fontAwesome
     },
     image:{}
 }
