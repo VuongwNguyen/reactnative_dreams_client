4d5b91a80d5a78964389bd61f17cc8e9c5439907
Nguyen Quoc Vuong <nguyenvuongw134@gmail.com>
2024-08-02 00:00:06 +0700

    fix: changed Asset variable (#17)

--- src/navigations/Navigator.js
+++ src/navigations/Navigator.js
@@ -1,13 +1,14 @@
 import {createStackNavigator} from '@react-navigation/stack';
 import {stackName} from './screens';
 import Counter from '../example/Counter';
+import Example from '../example/Example';
 
 const Stack = createStackNavigator();
 
 export function Navigator() {
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
-      <Stack.Screen name={stackName.example} component={Counter} />
+      <Stack.Screen name={stackName.example} component={Example} />
     </Stack.Navigator>
   );
 }
