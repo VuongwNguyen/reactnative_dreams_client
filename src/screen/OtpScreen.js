f4d3ccbd269e5b58b33088934cd153138b47c12c
VietTien198 <tienmp038@gmail.com>
2024-08-01 07:07:26 +0700

    delete forgotscreen

--- src/screen/OtpScreen.js
+++ src/screen/OtpScreen.js
@@ -92,36 +92,4 @@ const styles = StyleSheet.create({
     spacingHeight: {
         height: 40,
     },
-    textEmailOrPhone: {
-        color: 'black',
-        fontSize: 15,
-        letterSpacing: 0,
-        lineHeight: 25,
-        fontWeight: "700"
-    },
-    wraperInputContainer: {
-        width: "100%",
-        flexDirection: "row",
-        gap: 15,
-        alignItems: "center",
-        paddingVertical: 23,
-        paddingHorizontal: 10,
-        justifyContent: "center"
-    },
-    textInput: {
-        width: 50,
-        height: 60,
-        borderWidth: 1,
-        borderRadius: 10,
-        textAlign: "center",
-        fontSize: 30,
-        fontWeight: "700",
-        color: "black",
-        textShadowColor: "rgba(0, 0, 0, 0.25)",
-        textShadowOffset: {
-            width: 0,
-            height: 4
-        },
-        textShadowRadius: 4
-    }
 });
