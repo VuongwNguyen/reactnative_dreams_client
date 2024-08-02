9688c31a123085d5e5bea7073ca2a7f5b1740b63
Bùi Hữu Nghĩa <130369258+buihuunghia2004@users.noreply.github.com>
2024-08-02 09:46:41 +0700

    style: resolve conflic

--- src/styles/app/Assets.js
+++ src/styles/app/Assets.js
@@ -1,29 +1,29 @@
 /* icon assets by fontawesome6 library */
 export const Assets = {
-    icon: {
-        close: 'close',
-        eyeClose: 'eye-slash',
-        eyeOpen: 'eye',
-        search: 'search',// feather
-        user: 'user',
-        back: 'arrow-left',// feather
-        nationaly: 'earth-asia',
-        notification: 'bell', 
-        message: 'comment', // foundation
-        logout: 'log-out',// feather 
-        home: 'home', // entypo
-        translate: 'translate', // materialIcons
-        edit: 'edit', // materialIcons
-        location: 'location',// EvilIcons
-        gender: 'transgender', // fontAwesome
-        terms: 'archie', // feather
-        privacy: 'lock',
-        theme: 'theme-light-dark', // MaterialCommunityIcons
-        birthday: 'birthday-cake',// fontAwesome
-    },
-    image:{
-        logo:require('../../../assets/logo.png'),
-        google:require('../../../assets/google.png'),
-        github:require('../../../assets/github.png')
-    }
+  icon: {
+    close: 'close',
+    eyeClose: 'eye-slash',
+    eyeOpen: 'eye',
+    search: 'search',// feather
+    user: 'user',
+    back: 'arrow-left',// feather
+    nationaly: 'earth-asia',
+    notification: 'bell', 
+    message: 'comment', // foundation
+    logout: 'log-out',// feather 
+    home: 'home', // entypo
+    translate: 'translate', // materialIcons
+    edit: 'edit', // materialIcons
+    location: 'location',// EvilIcons
+    gender: 'transgender', // fontAwesome
+    terms: 'archie', // feather
+    privacy: 'lock',
+    theme: 'theme-light-dark', // MaterialCommunityIcons
+    birthday: 'birthday-cake',// fontAwesome
+  },
+  image:{
+    logo:require('../../../assets/logo.png'),
+    google:require('../../../assets/google.png'),
+    github:require('../../../assets/github.png')
+  }
 };
