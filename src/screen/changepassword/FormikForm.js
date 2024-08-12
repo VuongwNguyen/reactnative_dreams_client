8d881e6d1af17bfc9c42e645614df93f6f082a1f
Cheese2004 <133746473+Cheese2004@users.noreply.github.com>
2024-08-13 00:03:44 +0700

    feat: add reset-password api

--- src/screen/changepassword/FormikForm.js
+++ src/screen/changepassword/FormikForm.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, ToastAndroid, View} from 'react-native';
 import {changePasswordStyle} from '../../styles/changepassword/ChangePasswordStyle';
 import AppInput from '../../components/Input';
 import AppButton from '../../components/Button';
 import {useTranslation} from 'react-i18next';
-import { Typography } from '../../styles';
+import {Typography} from '../../styles';
 import {useFormikH} from '../../configs/hooks/useFormikH';
-import { ChangePasswordSchema } from '../../configs/validateSchema/ChangePasswordSchema';
+import {ChangePasswordSchema} from '../../configs/validateSchema/ChangePasswordSchema';
+import {useDispatch} from 'react-redux';
+import {APIResetPassword} from '../../store/api/AccountAPI';
 const FormikForm = () => {
   const {t} = useTranslation();
+  const emailReset = 'kimchi220204@gmail.com';
 
+  const dispatch = useDispatch();
   const {handleSubmit, handleChange, values, errors, touched} = useFormikH(
     {
       currentPassword: '',
@@ -18,8 +22,20 @@ const FormikForm = () => {
     },
     ChangePasswordSchema,
     (values, {resetForm}) => {
-      console.log(values);
-      resetForm();
+      dispatch(
+        APIResetPassword({
+          email: emailReset,
+          newPassword: values.newPassword,
+        }),
+      )
+        .unwrap()
+        .then(res => {
+          ToastAndroid.show('Cập nhật mật khẩu thành công', ToastAndroid.SHORT);
+          resetForm();
+        })
+        .catch(err => {
+          ToastAndroid.show(err.message, ToastAndroid.SHORT);
+        });
     },
   );
 
@@ -32,9 +48,7 @@ const FormikForm = () => {
           placeholder={t('changePwScreen.currentPw')}
         />
         {errors.currentPassword && (
-          <Text style={Typography.errorText}>
-            {errors.currentPassword}
-          </Text>
+          <Text style={Typography.errorText}>{errors.currentPassword}</Text>
         )}
       </View>
 
@@ -45,9 +59,7 @@ const FormikForm = () => {
           placeholder={t('changePwScreen.newPw')}
         />
         {errors.newPassword && (
-          <Text style={Typography.errorText}>
-            {errors.newPassword}
-          </Text>
+          <Text style={Typography.errorText}>{errors.newPassword}</Text>
         )}
       </View>
 
@@ -58,9 +70,7 @@ const FormikForm = () => {
           placeholder={t('changePwScreen.confirmNewPw')}
         />
         {errors.passwordConfirm && (
-          <Text style={Typography.errorText}>
-            {errors.passwordConfirm}
-          </Text>
+          <Text style={Typography.errorText}>{errors.passwordConfirm}</Text>
         )}
       </View>
 
