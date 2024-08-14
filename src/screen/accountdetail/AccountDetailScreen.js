4978cc86e3d9d560550f6cdd03a038a9de320b9f
Cheese2004 <133746473+Cheese2004@users.noreply.github.com>
2024-08-14 19:01:58 +0700

    feat: add image picker function

--- src/screen/accountdetail/AccountDetailScreen.js
+++ src/screen/accountdetail/AccountDetailScreen.js
@@ -5,13 +5,15 @@ import {
   TouchableOpacity,
   Image,
   Modal,
+  TouchableWithoutFeedback,
 } from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import {accountDetailStyle} from '../../styles/accountdetail/AccountDetailStyle';
 import {useTranslation} from 'react-i18next';
 import AppHeader from '../../components/Header';
 import TagInf from './TagInf';
 import {Assets} from '../../styles';
+import useImagePicker from './ImagePickerAvt';
 const basicInfArr = [
   {title: 'Title', icon: Assets.icon.user, content: 'content'},
   {title: 'Title', icon: Assets.icon.user, content: 'content'},
@@ -58,11 +60,13 @@ const showOtherInf = () => {
 
 const AccountDetailScreen = () => {
   const {t} = useTranslation();
+  const {image, setImage, openImageLibrary, onOpenCamera} = useImagePicker();
+  const [modalVisible, setModalVisible] = useState(false);
 
   const goBackScreen = () => {};
   const onSave = () => {};
   return (
-    <ScrollView style={{flex: 1}} showsVerticalScrollIndicator={false}>
+    <ScrollView showsVerticalScrollIndicator={false}>
       <View style={accountDetailStyle.container}>
         <AppHeader
           title={t('accountDetailScreen.infTitle')}
@@ -71,12 +75,79 @@ const AccountDetailScreen = () => {
 
         <View style={accountDetailStyle.bodyContainer}>
           <View style={accountDetailStyle.avtContainer}>
-            <Image source={Assets.image.avt} style={accountDetailStyle.avt} />
-            <TouchableOpacity style={accountDetailStyle.changeAvtButton}>
+            <Image
+              source={{
+                uri:
+                  image?.[0]?.uri ||
+                  'https://i.pinimg.com/564x/0f/78/5d/0f785d55cea2a407ac8c1d0c6ef19292.jpg',
+              }}
+              style={accountDetailStyle.avt}
+            />
+            <TouchableOpacity
+              style={accountDetailStyle.changeAvtButton}
+              onPress={() => {
+                setModalVisible(true);
+              }}>
               <Image source={Assets.image.ic_change} />
             </TouchableOpacity>
           </View>
-
+          {/* View modal */}
+          <Modal
+            animationType="fade"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              setModalVisible(!modalVisible);
+            }}>
+            <TouchableWithoutFeedback onPress={() => setModalVisible(false)}>
+              <View style={accountDetailStyle.modalView}>
+                <View style={accountDetailStyle.centeredView}>
+                  <Text style={accountDetailStyle.modalTitle}>
+                    Profile photo
+                  </Text>
+                  <View style={accountDetailStyle.optionContainer}>
+                    <TouchableOpacity
+                      style={accountDetailStyle.optionItem}
+                      onPress={() => {
+                        onOpenCamera();
+                        setModalVisible(false);
+                      }}>
+                      <Image
+                        source={Assets.image.ic_camera}
+                        style={accountDetailStyle.optionIconSize}
+                      />
+                      <Text style={accountDetailStyle.optionText}>Camera</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                      style={accountDetailStyle.optionItem}
+                      onPress={() => {
+                        openImageLibrary();
+                        setModalVisible(false);
+                      }}>
+                      <Image
+                        source={Assets.image.ic_gallery}
+                        style={accountDetailStyle.optionIconSize}
+                      />
+                      <Text style={accountDetailStyle.optionText}>Gallery</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                      style={accountDetailStyle.optionItem}
+                      onPress={() => {
+                        setImage('');
+                        setModalVisible(false);
+                      }}>
+                      <Image
+                        source={Assets.image.ic_delete}
+                        style={accountDetailStyle.optionIconSize}
+                      />
+                      <Text style={accountDetailStyle.optionText}>Remove</Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              </View>
+            </TouchableWithoutFeedback>
+          </Modal>
+          {/*  */}
           <View style={accountDetailStyle.groupInfContainer}>
             <Text style={accountDetailStyle.typeInf}>
               {t('accountDetailScreen.basic')}
--- src/styles/accountdetail/AccountDetailStyle.js
+++ src/styles/accountdetail/AccountDetailStyle.js
@@ -1,4 +1,4 @@
-import {Colors, Spacings, Typography} from '../../styles';
+import {Colors, Fonts, Sizing, Spacings, Typography} from '../../styles';
 import {Dimensions} from 'react-native';
 const {width, height} = Dimensions.get('window');
 const avtSize = width * 0.32;
@@ -17,7 +17,6 @@ export const accountDetailStyle = {
 
   bodyContainer: {
     flex: 1,
-    flexDirection: 'column',
     gap: 30,
     marginTop: Spacings.lg,
   },
@@ -54,5 +53,58 @@ export const accountDetailStyle = {
   typeInf: {
     ...Typography.typeInf,
     Colors: Colors.secondary,
+  },
+  // modal styles
+
+  modalView: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  centeredView: {
+    position: 'absolute',
+    left: '12%',
+    top: 0.25 * height,
+    minWidth: '75%',
+    marginHorizontal: 'auto',
+    backgroundColor: Colors.white,
+    alignItems: 'center',
+    padding: 20,
+    gap: 15,
+    borderWidth: 2,
+    borderRadius: 12,
+    borderColor: Colors.background,
+    // Hiệu ứng đổ bóng cho iOS
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 4},
+    shadowOpacity: 0.3,
+    shadowRadius: 6,
+    // Hiệu ứng đổ bóng cho Android
+    elevation: 8,
+  },
+  modalTitle: {
+    ...Typography.tagInf,
+    color: Colors.black,
+    fontFamily: Fonts.inter.bold,
+  },
+  optionContainer: {
+    flexDirection: 'row',
+    gap: 20,
+    alignItems: 'center',
+  },
+  optionItem: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.background,
+    borderRadius: 12,
+    padding: 10,
+    gap: 8,
+  },
+  optionIconSize: {
+    width: Sizing.lg,
+    height: Sizing.lg,
+  },
+  optionText: {
+    ...Typography.tagInf,
+    color: Colors.secondary,
   },
 };
