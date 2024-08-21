32f20cbfefe5ddc9472b7f944bbc76b8edf585ce
VuongwNguyen <nguyenvuongw134@gmail.com>
2024-08-22 02:06:35 +0700

    feat: connect screens navigator

--- src/navigations/Navigator.js
+++ src/navigations/Navigator.js
@@ -1,21 +1,56 @@
-import { createStackNavigator } from '@react-navigation/stack';
-import { stackName } from './screens';
-import Counter from '../example/Counter';
-import Example from '../example/Example';
-
-import ForgotPasswordScreen from '../screen/forgotpasswordscreen/ForgotPasswordScreen';
-import ChangePasswordScreen from '../screen/changepassword/ChangePasswordScreen';
-import ChangeNewPasswordScreen from '../screen/changenewpassword/ChangeNewPasswordScreen';
-import HomeScreen from '../screen/homescreen/HomeScreen';
-import OtpScreen from '../screen/otpscreen/OtpScreen';
-import AccountDetailScreen from '../screen/accountdetail/AccountDetailScreen';
-import PostDetailScreen from '../screen/postdetailscreen/PostDetailScreen';
+import {createStackNavigator} from '@react-navigation/stack';
+import {stackName} from './screens';
 
 const Stack = createStackNavigator();
 export function Navigator() {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name={stackName.example} component={AccountDetailScreen} />
+    <Stack.Navigator
+      screenOptions={{headerShown: false}}
+      initialRouteName={stackName.bottomTab.name}>
+      <Stack.Screen
+        name={stackName.login.name}
+        component={stackName.login.component}
+      />
+      <Stack.Screen
+        name={stackName.register.name}
+        component={stackName.register.component}
+      />
+      <Stack.Screen
+        name={stackName.profile.name}
+        component={stackName.profile.component}
+      />
+      <Stack.Screen
+        name={stackName.postDetail.name}
+        component={stackName.postDetail.component}
+      />
+      <Stack.Screen
+        name={stackName.accountDetail.name}
+        component={stackName.accountDetail.component}
+      />
+      <Stack.Screen
+        name={stackName.changePassword.name}
+        component={stackName.changePassword.component}
+      />
+      <Stack.Screen
+        name={stackName.changeNewPassword.name}
+        component={stackName.changeNewPassword.component}
+      />
+      <Stack.Screen
+        name={stackName.forgotPassword.name}
+        component={stackName.forgotPassword.component}
+      />
+      <Stack.Screen
+        name={stackName.otp.name}
+        component={stackName.otp.component}
+      />
+      <Stack.Screen
+        name={stackName.newPost.name}
+        component={stackName.newPost.component}
+      />
+      <Stack.Screen
+        name={stackName.bottomTab.name}
+        component={stackName.bottomTab.component}
+      />
     </Stack.Navigator>
   );
 }
