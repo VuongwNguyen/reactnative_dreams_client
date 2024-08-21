4e6b2a5c7a0685f27e65019ed2175a2dd00a42ac
Nguyen Quoc Vuong <nguyenvuongw134@gmail.com>
2024-08-22 02:08:30 +0700

    Vuong/review code (#42)
    * refactor: split composenent tagInf
    
    * feat: connect screens navigator

--- src/navigations/screens.js
+++ src/navigations/screens.js
@@ -1,5 +1,37 @@
+import AccountDetailScreen from '../screen/accountdetail/AccountDetailScreen';
+import ChangeNewPasswordScreen from '../screen/changenewpassword/ChangeNewPasswordScreen';
+import ChangePasswordScreen from '../screen/changepassword/ChangePasswordScreen';
+import ForgotPasswordScreen from '../screen/forgotpasswordscreen/ForgotPasswordScreen';
+import HomeScreen from '../screen/homescreen/HomeScreen';
+import OtpScreen from '../screen/otpscreen/OtpScreen';
+import PostDetailScreen from '../screen/postdetailscreen/PostDetailScreen';
+import ProfileScreen from '../screen/profileScreen/ProfileScreen';
+import LoginScreen from '../screen/loginScreen/LoginScreen';
+import RegisterScreen from '../screen/registerScreen/RegisterScreen';
+import NewPostScreen from '../screen/newpost/NewPostScreen';
+import BottomTab from '../navigations/BottomTab';
+
 export const stackName = {
   example: 'Example',
+  login: {name: 'login', component: LoginScreen},
+  register: {name: 'register', component: RegisterScreen},
+  profile: {name: 'profile', component: ProfileScreen},
+  postDetail: {name: 'postDetail', component: PostDetailScreen},
+  accountDetail: {name: 'accountDetail', component: AccountDetailScreen},
+  changePassword: {name: 'changePassword', component: ChangePasswordScreen},
+  changeNewPassword: {
+    name: 'changeNewPassword',
+    component: ChangeNewPasswordScreen,
+  },
+  forgotPassword: {name: 'forgotPassword', component: ForgotPasswordScreen},
+  otp: {name: 'otp', component: OtpScreen},
+  newPost: {name: 'newPost', component: NewPostScreen},
+  bottomTab: {name: 'bottomTab', component: BottomTab},
 };
 
-export const tab = {};
+export const tabName = {
+  home: {name: 'home', component: HomeScreen},
+  notification: {name: 'notification', component: HomeScreen},
+  search: {name: 'search', component: HomeScreen},
+  profile: {name: 'profile', component: HomeScreen},
+};
