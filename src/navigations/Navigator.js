00ac877f1f59449e77f77b666a9be93b0c103970
VietTien198 <141416494+VietTien198@users.noreply.github.com>
2024-08-24 22:24:45 +0700

    feat: add callscreen (#45)
    * feat:add apiSenOtpResetPW and apiVerifyCodeResetPW
    
    * Update Handle.js
    
    * feat: add HomeScreen
    
    * feat: tab followed
    
    * chore: change style post
    
    * chore: change language
    
    * feat: add postDetailScreen
    
    * feat: add language for postDetailScreen
    
    * chore: slipt commentitem
    
    * chore: edit homescreen
    
    * feat: CallScreen

--- src/navigations/Navigator.js
+++ src/navigations/Navigator.js
@@ -1,11 +1,25 @@
-import {createStackNavigator} from '@react-navigation/stack';
-import {stackName} from './screens';
+import { createStackNavigator } from '@react-navigation/stack';
+import { stackName } from './screens';
+import Counter from '../example/Counter';
+import Example from '../example/Example';
+
+import ForgotPasswordScreen from '../screen/forgotpasswordscreen/ForgotPasswordScreen';
+import ChangePasswordScreen from '../screen/changepassword/ChangePasswordScreen';
+import ChangeNewPasswordScreen from '../screen/changenewpassword/ChangeNewPasswordScreen';
+import HomeScreen from '../screen/homescreen/HomeScreen';
+import OtpScreen from '../screen/otpscreen/OtpScreen';
+import AccountDetailScreen from '../screen/accountdetail/AccountDetailScreen';
+import PostDetailScreen from '../screen/postdetailscreen/PostDetailScreen';
+import CallScreen from '../screen/callscreen/CallScreen';
+
+import { createStackNavigator } from '@react-navigation/stack';
+import { stackName } from './screens';
 
 const Stack = createStackNavigator();
 export function Navigator() {
   return (
     <Stack.Navigator
-      screenOptions={{headerShown: false}}
+      screenOptions={{ headerShown: false }}
       initialRouteName={stackName.bottomTab.name}>
       <Stack.Screen
         name={stackName.login.name}
