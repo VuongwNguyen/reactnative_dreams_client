40207811cb95b1fc135d687fa5768ff3ef28362f
VietTien198 <tienmp038@gmail.com>
2024-08-24 14:58:06 +0700

    feat: add MessageScreen

--- src/navigations/Navigator.js
+++ src/navigations/Navigator.js
@@ -10,12 +10,14 @@ import HomeScreen from '../screen/homescreen/HomeScreen';
 import OtpScreen from '../screen/otpscreen/OtpScreen';
 import AccountDetailScreen from '../screen/accountdetail/AccountDetailScreen';
 import PostDetailScreen from '../screen/postdetailscreen/PostDetailScreen';
+import MessageScreen from '../screen/chatscreen/MessageScreen';
+
 
 const Stack = createStackNavigator();
 export function Navigator() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name={stackName.example} component={HomeScreen} />
+      <Stack.Screen name={stackName.example} component={MessageScreen} />
     </Stack.Navigator>
   );
 }
