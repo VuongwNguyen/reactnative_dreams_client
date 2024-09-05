6c4ef972c76ab696636c0a2498d1e43a5ccaa740
Bùi Hữu Nghĩa <130369258+buihuunghia2004@users.noreply.github.com>
2024-09-05 09:47:31 +0700

    Nghia/fling (#55)
    * feat: policy privacy screen
    
    * style: drop down
    
    * feat: settings
    
    * feat: fling screen

---
+++ src/screen/flingscreen/FlingScreen.js
@@ -0,0 +1,56 @@
+import {FlatList, Image, Text, TouchableOpacity, View} from 'react-native';
+import React from 'react';
+import {FlingStyle} from '../../styles/flingstyle/FlingStyle';
+import Header from '../../components/Header';
+const data = [
+  {
+    name: 'Fling 1',
+    folowwing: true,
+  },
+  {
+    name: 'Fling 1',
+    folowwing: true,
+  },
+  {
+    name: 'Fling 1',
+    folowwing: false,
+  },
+  {
+    name: 'Fling 1',
+    folowwing: false,
+  },
+  {
+    name: 'Fling 1',
+    folowwing: true,
+  },
+  {
+    name: 'Fling 1',
+    folowwing: true,
+  },
+];
+const FlingScreen = () => {
+  return (
+    <View style={FlingStyle.container}>
+      <Header title={'Fling Screen'} />
+      <FlatList
+        data={data}
+        renderItem={({item}) => (
+          <View style={FlingStyle.item}>
+            <Image
+              style={FlingStyle.avatar}
+              source={{
+                uri: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/d07bca98931623.5ee79b6a8fa55.jpg',
+              }}
+            />
+            <Text style={FlingStyle.name}>{item.name}</Text>
+            <TouchableOpacity style={item.folowwing ? FlingStyle.flowwing : FlingStyle.flowwed}>
+              <Text style={item.folowwing ? FlingStyle.flowwingText : FlingStyle.flowwedText}>{item.folowwing ? 'Followed' : 'Follow'}</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      />
+    </View>
+  );
+};
+
+export default FlingScreen;
---
+++ src/styles/flingstyle/FlingStyle.js
@@ -0,0 +1,56 @@
+import { StyleSheet } from 'react-native'
+import { Colors } from '../app/Colors'
+export const FlingStyle = StyleSheet.create({
+  container:{
+    flex:1,
+    padding:12
+  },
+  avatar:{
+    width:50,
+    height:50,
+    borderRadius:25
+  },
+  item:{
+    flexDirection:'row',
+    height:60,
+    marginTop:10,
+    alignContent:'center',
+    marginHorizontal:12,
+    alignItems:'center'
+  },
+  name:{
+    fontSize:16,
+    fontWeight:'bold',
+    color:'black',
+    marginTop:5,
+    marginBottom:5,
+    flex:1,
+    paddingHorizontal:12
+  },
+  flowwing:{
+    height:24,
+    width:100,
+    backgroundColor:Colors.primary,
+    borderRadius:12,
+    justifyContent:'center'
+  },
+  flowwed:{
+    height:24,
+    width:100,
+    borderRadius:12,
+    borderWidth:1,
+    borderColor:'black',
+    justifyContent:'center'
+  },
+  flowwedText:{
+    fontSize:12,
+    color:Colors.primary,
+    textAlign:'center'
+  },
+  flowwingText:{
+    fontSize:12,
+    color:'white',
+    textAlign:'center',
+    alignSelf:'center'
+  }
+})
