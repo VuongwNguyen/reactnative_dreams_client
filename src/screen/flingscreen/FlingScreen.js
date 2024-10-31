6e94327f849eb3f95ec6dba46846cf1bd1c2951d
VietTien198 <tienmp038@gmail.com>
2024-10-31 16:38:10 +0700

    feat: add api_followers and api_following

--- src/screen/flingscreen/FlingScreen.js
+++ src/screen/flingscreen/FlingScreen.js
@@ -1,61 +1,74 @@
-import {FlatList, Image, Text, TouchableOpacity, View} from 'react-native';
-import React from 'react';
-import {FlingStyle} from '../../styles/flingstyle/FlingStyle';
+import { FlatList, Image, Text, TouchableOpacity, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { FlingStyle } from '../../styles/flingstyle/FlingStyle';
 import Header from '../../components/Header';
-const data = [
-  {
-    name: 'Fling 1',
-    folowwing: true,
-  },
-  {
-    name: 'Fling 1',
-    folowwing: true,
-  },
-  {
-    name: 'Fling 1',
-    folowwing: false,
-  },
-  {
-    name: 'Fling 1',
-    folowwing: false,
-  },
-  {
-    name: 'Fling 1',
-    folowwing: true,
-  },
-  {
-    name: 'Fling 1',
-    folowwing: true,
-  },
-];
-const FlingScreen = () => {
+import { useDispatch } from 'react-redux';
+import { APIGetFollowers, APIGetFollowing } from '../../store/api/FollowAPI';
+import { current } from '@reduxjs/toolkit';
+
+const FlingScreen = (props) => {
+  const params = props.route.params
+  const type = params?.type
+  const user_id_view = params?.user_id_view
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const dispatch = useDispatch();
+
+  const fetchData = async () => {
+    try {
+      setIsLoading(true);
+      if (type === 'Followers') {
+        const result = await dispatch(APIGetFollowers({ user_id_view, page })).unwrap();
+        setData(result.list);
+      }
+      if (type === 'Following') {
+        const result = await dispatch(APIGetFollowing({ user_id_view, page })).unwrap();
+        setData(result.list);
+      }
+    } catch (error) {
+      setIsLoading(false)
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [page]);
+
+  const RenderItems = ({ item }) => {
+    return (
+      <View style={FlingStyle.item}>
+        <Image
+          style={FlingStyle.avatar}
+          source={{
+            uri: item.follower?.avatar || item.user.avatar
+          }}
+        />
+        <Text style={FlingStyle.name}>{item.follower?.fullname || item.user?.fullname}</Text>
+        <TouchableOpacity
+          style={item.isFollowing ? FlingStyle.flowwing : FlingStyle.flowwed}>
+          <Text
+            style={
+              item.isFollowing
+                ? FlingStyle.flowwingText
+                : FlingStyle.flowwedText
+            }>
+            {item.isFollowing ? 'Followed' : 'Follow'}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+
   return (
     <View style={FlingStyle.container}>
-      <Header title={'Fling Screen'} />
+      <Header title={type} />
       <FlatList
         data={data}
-        renderItem={({item}) => (
-          <View style={FlingStyle.item}>
-            <Image
-              style={FlingStyle.avatar}
-              source={{
-                uri: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/d07bca98931623.5ee79b6a8fa55.jpg',
-              }}
-            />
-            <Text style={FlingStyle.name}>{item.name}</Text>
-            <TouchableOpacity
-              style={item.folowwing ? FlingStyle.flowwing : FlingStyle.flowwed}>
-              <Text
-                style={
-                  item.folowwing
-                    ? FlingStyle.flowwingText
-                    : FlingStyle.flowwedText
-                }>
-                {item.folowwing ? 'Followed' : 'Follow'}
-              </Text>
-            </TouchableOpacity>
-          </View>
-        )}
+        renderItem={({ item }) => <RenderItems item={item} />}
+        keyExtractor={(item, index) => index.toString()}
       />
     </View>
   );
