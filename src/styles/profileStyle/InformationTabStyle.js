d7114948f37db5dae0beec090a38af28296380ab
Cheese2004 <133746473+Cheese2004@users.noreply.github.com>
2024-11-02 20:34:01 +0700

    refactor: add padding for inf tag

--- src/styles/profileStyle/InformationTabStyle.js
+++ src/styles/profileStyle/InformationTabStyle.js
@@ -1,17 +1,18 @@
 import {StyleSheet} from 'react-native';
 import {Typography} from '../app/Typographys';
 import {Colors} from '../app/Colors';
-import { Fonts } from '../app/Fonts';
+import {Fonts} from '../app/Fonts';
 export const InfomationTabStyle = StyleSheet.create({
   placeholder: {
     margin: 'auto',
-    fontFamily:Fonts.roboto.regular,
-    fontSize:18,
-    color:'black'
+    fontFamily: Fonts.roboto.regular,
+    fontSize: 18,
+    color: 'black',
   },
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    paddingHorizontal: 12,
   },
   rowContainer: {
     flexDirection: 'row',
