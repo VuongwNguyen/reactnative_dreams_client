918a80e93d54143713118a0e90e4227afbad9da9
Cheese2004 <133746473+Cheese2004@users.noreply.github.com>
2024-11-05 17:44:26 +0700

    refactor: fix conflict into main

--- src/screen/profile/PostedTab.js
+++ src/screen/profile/PostedTab.js
@@ -5,14 +5,14 @@ import {
   ToastAndroid,
   View,
 } from 'react-native';
-import React, {useEffect, useRef, useState,useCallback} from 'react';
+import React, {useEffect, useRef, useState, useCallback} from 'react';
 import ItemPost, {ItemSeparator} from '../../components/ItemPost';
 import {useDispatch} from 'react-redux';
 import {useFocusEffect} from '@react-navigation/native';
 import {PostedTabStyle} from '../../styles/profileStyle/PostedTabStyle';
 import {APIGetPostByUser} from '../../store/api/PostAPI';
 import Animated from 'react-native-reanimated';
-import { Colors } from '../../styles';
+import {Colors} from '../../styles';
 
 const PostedTab = props => {
   const {scrollHandler, user_id_view} = props;
@@ -21,12 +21,16 @@ const PostedTab = props => {
   const [viewedItemIds, setViewedItemIds] = useState([]);
   const [timeOutId, setTimeOutId] = useState(null);
   const [refreshing, setRefreshing] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [page, setPage] = useState({});
+  const [nextPage, setNextPage] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchPosts = () => {
     dispatch(APIGetPostByUser({user_id_view}))
       .unwrap()
       .then(res => {
-        const { list, page } = res;
+        const {list, page} = res;
         setNextPage(res);
         setPage(page);
         if (currentPage === 1) {
@@ -47,7 +51,8 @@ const PostedTab = props => {
   useFocusEffect(
     useCallback(() => {
       fetchPosts();
-    }, [user_id_view])
+      setRefreshing(true);
+    }, [user_id_view, currentPage]),
   );
 
   const onRefresh = () => {
@@ -58,35 +63,29 @@ const PostedTab = props => {
   const onEndReached = useCallback(() => {
     if (currentPage < page.maxPage && !isLoading) {
       setCurrentPage(prevPage => prevPage + 1);
-      setNextPage(true)
+      setNextPage(true);
     }
   }, [currentPage, nextPage, isLoading, dispatch]);
 
   const renderLoader = () => {
-    return isLoading ? <ActivityIndicator size="large" color={Colors.primary} /> : null;
+    return isLoading ? (
+      <ActivityIndicator size="large" color={Colors.primary} />
+    ) : null;
   };
 
   return (
     <View style={PostedTabStyle.container}>
-      {loading ? (
-        <ActivityIndicator
-          size="large"
-          color="#ededed"
-          style={{justifyContent: 'center'}}
-        />
-      ) : (
-        <Animated.FlatList
-          onScroll={scrollHandler}
-          data={dataPosts}
-          showsVerticalScrollIndicator={false}
-          renderItem={({item}) => <ItemPost item={item} />}
-          keyExtractor={item => item._id}
-          ItemSeparatorComponent={() => <ItemSeparator />}
-          refreshControl={
-            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-          }
-        />
-      )}
+      <Animated.FlatList
+        onScroll={scrollHandler}
+        data={dataPosts}
+        showsVerticalScrollIndicator={false}
+        renderItem={({item}) => <ItemPost item={item} />}
+        keyExtractor={item => item._id}
+        ItemSeparatorComponent={() => <ItemSeparator />}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      />
     </View>
   );
 };
