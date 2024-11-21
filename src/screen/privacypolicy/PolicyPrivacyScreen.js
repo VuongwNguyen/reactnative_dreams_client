56348e138496afd321ea96a2574b9d26d24db1d7
Nguyen Quoc Vuong <nguyenvuongw134@gmail.com>
2024-11-21 20:44:01 +0700

    vuong/policy (#111)
    * feat: render policy
    
    * feat: link server

--- src/screen/privacypolicy/PolicyPrivacyScreen.js
+++ src/screen/privacypolicy/PolicyPrivacyScreen.js
@@ -1,59 +1,33 @@
-import {View} from 'react-native';
-import React from 'react';
+import {FlatList, View} from 'react-native';
+import React, {useState, useEffect} from 'react';
 import Header from '../../components/Header';
 import DropDownText from '../../components/DropDownText';
 import PolicyPrivacyStyle from '../../styles/policyprivacystyle/PolicyPrivacyStyle';
-
-const data = [
-  {
-    title: '5 điều Bác Hồ dạy',
-    contents: [
-      '1.Yêu tổ quốc yêu đồng bào',
-      '2.Học tập tốt lao động tốt',
-      '3.Đoàn kết tốt kỷ luật tốt',
-      '4.Giữ gìn vệ sinh thật tốt',
-      '5.Khiêm tốn thật thà dũng cảm',
-    ],
-  },
-  {
-    title: '5 điều Bác Hồ dạy',
-    contents: [
-      '1.Yêu tổ quốc yêu đồng bào',
-      '2.Học tập tốt lao động tốt',
-      '3.Đoàn kết tốt kỷ luật tốt',
-      '4.Giữ gìn vệ sinh thật tốt',
-      '5.Khiêm tốn thật thà dũng cảm',
-    ],
-  },
-  {
-    title: '5 điều Bác Hồ dạy',
-    contents: [
-      '1.Yêu tổ quốc yêu đồng bào',
-      '2.Học tập tốt lao động tốt',
-      '3.Đoàn kết tốt kỷ luật tốt',
-      '4.Giữ gìn vệ sinh thật tốt',
-      '5.Khiêm tốn thật thà dũng cảm',
-    ],
-  },
-  {
-    title: '5 điều Bác Hồ dạy',
-    contents: [
-      '1.Yêu tổ quốc yêu đồng bào',
-      '2.Học tập tốt lao động tốt',
-      '3.Đoàn kết tốt kỷ luật tốt',
-      '4.Giữ gìn vệ sinh thật tốt',
-      '5.Khiêm tốn thật thà dũng cảm',
-    ],
-  },
-];
-
+import AxiosInstance from '../../configs/axiosInstance';
+import { useTranslation } from 'react-i18next';
 const PolicyPrivacyScreen = () => {
+  const { t } = useTranslation();
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    AxiosInstance()
+      .get('/policy/policy')
+      .then(res => {
+        setData(res.data);
+      });
+  }, []);
+
   return (
     <View style={PolicyPrivacyStyle.container}>
-      <Header title={'Policy Privacy'} />
-      {data.map((item, index) => (
-        <DropDownText key={index} title={item.title} contents={item.contents} />
-      ))}
+      <Header title={t('policyScreen.title')} />
+      <FlatList
+        style={{flex: 1}}
+        showsVerticalScrollIndicator={false}
+        data={data}
+        renderItem={({item}) => (
+          <DropDownText title={item.title} contents={item.children} />
+        )}
+        keyExtractor={item => item._id}
+      />
     </View>
   );
 };
