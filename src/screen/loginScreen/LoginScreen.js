03fd1864df251f9c9d4fae38c68e84422724e23f
VuongwNguyen <nguyenvuongw134@gmail.com>
2024-12-08 19:57:26 +0700

    feat: login github

--- src/screen/loginScreen/LoginScreen.js
+++ src/screen/loginScreen/LoginScreen.js
@@ -7,45 +7,63 @@ import {
   View,
   ActivityIndicator,
 } from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import {Assets, Colors} from '../../styles';
 import {useTranslation} from 'react-i18next';
 import FormikForm from './FormikForm';
 import {LoginStyle} from '../../styles/loginStyle/LoginStyle';
 import {stackName} from '../../navigations/screens';
 import {useSelector} from 'react-redux';
+import {WebView} from 'react-native-webview';
 
-const LoginScreen = props => {
+const LoginScreen = ({navigation}) => {
   const {t} = useTranslation();
   const {ggSigninLoading} = useSelector(state => state.account);
+  const [github, setGithub] = useState(false);
 
-  const {navigation} = props;
   return (
     <KeyboardAvoidingView style={LoginStyle.viewContainer}>
-      <ScrollView showsVerticalScrollIndicator={false} scrollEnabled={false}>
-        <View style={LoginStyle.container}>
-          <View style={LoginStyle.containerHeader}>
-            <Image style={LoginStyle.logo} source={Assets.image.logo} />
-            <Text style={LoginStyle.headerText}>
-              {t('loginScreen.welcome')}
-            </Text>
-            <Text style={LoginStyle.subTitle}>{t('loginScreen.subTitle')}</Text>
-            <Text style={LoginStyle.titleText}>{t('loginScreen.title')}</Text>
-          </View>
-          <View>
-            <FormikForm />
-          </View>
-          <View style={LoginStyle.containerLink}>
-            <Text>{t('loginScreen.notAccount')}</Text>
-            <TouchableOpacity
-              onPress={() => {
-                navigation.navigate(stackName.register.name);
-              }}>
-              <Text style={LoginStyle.link}>{t('loginScreen.register')}</Text>
-            </TouchableOpacity>
-          </View>
+      {github ? (
+        <View>
+          <TouchableOpacity style={{alignSelf: 'flex-end'}}>
+            <Text>Huỷ</Text>
+          </TouchableOpacity>
+          <WebView
+            source={{uri: ''}}
+            style={{
+              width: '100%',
+              height: '100%',
+            }}
+          />
         </View>
-      </ScrollView>
+      ) : (
+        <ScrollView showsVerticalScrollIndicator={false} scrollEnabled={false}>
+          <View style={LoginStyle.container}>
+            <View style={LoginStyle.containerHeader}>
+              <Image style={LoginStyle.logo} source={Assets.image.logo} />
+              <Text style={LoginStyle.headerText}>
+                {t('loginScreen.welcome')}
+              </Text>
+              <Text style={LoginStyle.subTitle}>
+                {t('loginScreen.subTitle')}
+              </Text>
+              <Text style={LoginStyle.titleText}>{t('loginScreen.title')}</Text>
+            </View>
+            <View>
+              <FormikForm setGithub={setGithub} />
+            </View>
+            <View style={LoginStyle.containerLink}>
+              <Text>{t('loginScreen.notAccount')}</Text>
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.navigate(stackName.register.name);
+                }}>
+                <Text style={LoginStyle.link}>{t('loginScreen.register')}</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </ScrollView>
+      )}
       {ggSigninLoading && (
         <View
           style={{
